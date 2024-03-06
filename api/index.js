6f3ba3d0749ea78196e7e06732cdaf455b7d1235
Mrunmay Chichkhede <mchichkhede2002@gmail.com>
2024-03-06 12:28:43 +0530

    Added Server

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connected to db!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000!");
